f865c0b6025929381746ecbd95e21afb173bdf19
James <sirtopas@gmail.com>
2021-05-30 21:17:51 +0100

    build: clear repo

--- README.md
+++
@@ -1,5 +0,0 @@
-# Topify
-
-Uses the Spotify API and OAuth to log users in and get their top artists and songs!
-
-Built using Angular12. 
--- src/app/shared/spotify-auth-response.ts
+++
@@ -1,6 +0,0 @@
-export class SpotifyAuthResponse {
-    access_token!: string;
-    expires_in!: number;
-    public state: string | undefined;
-    public token_type: string | undefined;
-}
